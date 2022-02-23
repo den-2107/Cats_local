d302b8f7e647853b821bcbb3f685d9539d898787
den-2107 <stalkerden69@gmail.com>
2022-02-23 23:01:35 +0300

    add

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const containers = document.querySelectorAll(".card");
 containers.forEach((container) => {
     container.addEventListener("click", (e) => {
-        const popup = container.querySelector(".info-container");
+        const popup = container.querySelector(".container");
+        console.log(popup);
         popup.classList.add("active");
 
     })
