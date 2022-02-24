1b2984f46c4e3ce13f7cee7c534c271ffd0acdae
den-2107 <stalkerden69@gmail.com>
2022-02-24 14:11:40 +0300

    patch_3

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ containers.forEach((container) => {
         const popup = container.querySelector(".container");
         console.log(popup);
         popup.classList.add("active");
-
         containers.forEach(c => c.classList.remove("card_hover"));
-    })
+        
+        const cross = popup.querySelector(".close");
+        cross.addEventListener("click", (e) => {
+            e.stopPropagation();
+            popup.classList.remove("active");
+            containers.forEach(c => c.classList.add("card_hover"));
+        });
+    });
 });
