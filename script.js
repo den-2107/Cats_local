da65e1a9cd22527cc7a4dc29504a20eb7aa2721e
den-2107 <stalkerden69@gmail.com>
2022-02-24 19:34:18 +0300

    patch_4

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const containers = document.querySelectorAll(".card");
-containers.forEach((container) => {
-    container.addEventListener("click", (e) => {
-        const popup = container.querySelector(".container");
-        console.log(popup);
-        popup.classList.add("active");
-        containers.forEach(c => c.classList.remove("card_hover"));
-        
-        const cross = popup.querySelector(".close");
-        cross.addEventListener("click", (e) => {
-            e.stopPropagation();
-            popup.classList.remove("active");
-            containers.forEach(c => c.classList.add("card_hover"));
-        });
+const cards = document.querySelectorAll(".card");
+const infoCats = document.querySelector(".info-cats")
+
+document.getElementById("lara-card").addEventListener("click", (e) => setClickEvent("lara-popup"));
+document.getElementById("bazil-card").addEventListener("click", (e) => setClickEvent("bazil-popup"));
+document.getElementById("rish-card").addEventListener("click", (e) => setClickEvent("rish-popup"));
+document.getElementById("elli-card").addEventListener("click", (e) => setClickEvent("elli-popup"));
+document.getElementById("charlye-card").addEventListener("click", (e) => setClickEvent("charlye-popup"));
+document.getElementById("stefani-card").addEventListener("click", (e) => setClickEvent("stefani-popup"));
+document.getElementById("dusya-card").addEventListener("click", (e) => setClickEvent("dusya-popup"));
+document.getElementById("bruno-card").addEventListener("click", (e) => setClickEvent("bruno-popup"));
+document.getElementById("svetlyachock-card").addEventListener("click", (e) => setClickEvent("svetlyachock-popup"));
+
+function setClickEvent(popupId) {
+    const popup = document.getElementById(popupId);
+    popup.classList.add("container_active");
+    infoCats.classList.add("info-cats_active");
+    const cross = popup.querySelector(".close");
+    cross.addEventListener("click", (e) => {
+        e.stopPropagation();
+        popup.classList.remove("container_active");
+        infoCats.classList.remove("info-cats_active");
     });
-});
+}
