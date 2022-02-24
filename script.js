9d96ee31bef5f9f601bb2279658ab984a50db2b8
Kerasirov E. D <KerasirovED@ya.ru>
2022-02-24 11:48:42 +0300

    Fixed card hover issue

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ containers.forEach((container) => {
         const popup = container.querySelector(".container");
         console.log(popup);
         popup.classList.add("active");
-        popup.classList.remove("card:hover");
 
+        containers.forEach(c => c.classList.remove("card_hover"));
     })
 });
